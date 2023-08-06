da4cb444eea781105bf4de2908f241255e1d55f2
Ikko Eltociear Ashimine <eltociear@gmail.com>
2023-08-07 00:15:54 +0900

    fix typo in page.tsx
    Threds -> Threads

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -24,7 +24,7 @@ async function Page() {
     <main className='mx-auto flex max-w-3xl flex-col justify-start px-10 py-20'>
       <h1 className='head-text'>Onboarding</h1>
       <p className='mt-3 text-base-regular text-light-2'>
-        Complete your profile now, to use Threds.
+        Complete your profile now, to use Threads.
       </p>
 
       <section className='mt-9 bg-dark-2 p-10'>
