86b2c9969905e5ad6014505b7676d8e6dc038366
zhangji <1453411403@qq.com>
2023-08-16 21:56:09 +0800

    fix: '/search' -> 'search'

--- components/shared/Searchbar.tsx
+++ components/shared/Searchbar.tsx
@@ -41,7 +41,7 @@ function Searchbar({ routeType }: Props) {
         value={search}
         onChange={(e) => setSearch(e.target.value)}
         placeholder={`${
-          routeType !== "/search" ? "Search communities" : "Search creators"
+          routeType !== "search" ? "Search communities" : "Search creators"
         }`}
         className='no-focus searchbar_input'
       />
